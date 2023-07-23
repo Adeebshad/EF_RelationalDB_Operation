2ec2fe36f8d99add594b32dce1b2d69355324f76
Adeebshad <csadeebshad@gmail.com>
2023-07-23 15:34:17 +0600

    first commit

---
+++ EF_RelationalDB_Crud/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using EF_RelationalDB_Crud.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_RelationalDB_Crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly MyWorldDbContext _myWorldDbContext;
+        public CustomerController(MyWorldDbContext myWorldDbContext)
+        {
+            _myWorldDbContext = myWorldDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var customers = await _myWorldDbContext.Customer
+            .Include(_ => _.CustomerAddresses).ToListAsync();
+            return Ok(customers);
+        }
+    }
+}
---
+++ EF_RelationalDB_Crud/Program.cs
@@ -0,0 +1,37 @@
+using EF_RelationalDB_Crud.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyWorldDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyWorldDbConnection"));
+});
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
